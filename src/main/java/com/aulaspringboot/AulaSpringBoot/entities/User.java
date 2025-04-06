e3a76616509a7205593e65ed975e335c3f0c72af
Dev-VitorPereira <dev.vitorpereira@gmail.com>
2025-04-06 00:01:52 -0300

    Feat: Criado ORM (objeto relacional)

--- src/main/java/com/aulaspringboot/AulaSpringBoot/entities/User.java
+++ src/main/java/com/aulaspringboot/AulaSpringBoot/entities/User.java
@@ -1,8 +1,6 @@
 package com.aulaspringboot.AulaSpringBoot.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,9 +14,16 @@ import lombok.NoArgsConstructor;
 @Table(name = "tb_user")
 public class User {
     @Id
-
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
+
+    @ManyToOne
+    @JoinColumn(name = "department_id")
     private  Department department;
+
+
+
+
 }
